99a29cf06e776784adc8ac7784468bfbe48c028c
Saurabh <suarbhbora7060@gmail.com>
2026-02-13 22:47:27 +0530

    Assignment

---
+++ RESTfulWebservicesAssignments/Part2/RESTfulWebservices/RESTfulWebservices/src/test/java/com/Bootcamp/RESTfulWebservices/ResTfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bootcamp.RESTfulWebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
