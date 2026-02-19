64fd9d83dabfe955d8e2f5f968df6f3bd3ce20ce
Hello-world-28 <saurabhbora7060@gmail.com>
2026-02-19 23:52:44 +0530

    Assignment

--- 3MasteringSpringBootRESTapiDevelopment/beckend/src/main/java/com/Learn/Spring/beckend/Controller/UserController.java
+++ 3MasteringSpringBootRESTapiDevelopment/beckend/src/main/java/com/Learn/Spring/beckend/Controller/UserController.java
@@ -11,6 +11,7 @@ public class UserController {
 
     @GetMapping(path={"/api/dummy/users/{userId}/post/{postId}", "/api/dummy/users/{userId}"})
     public String searchUserPostWithMultiplathVariables(@PathVariable long userId, @PathVariable(required=false) String postId) {
+<<<<<<< HEAD
         String response;
         if(postId == null){
             response = ("User id is : " + userId);
@@ -26,5 +27,9 @@ public class UserController {
     @GetMapping(path={"/api/dummy/users/{userId}/phone/{phNo}"})
     public String searchUserPhoneNumberWithMultiplathVariables(@PathVariable Map<String, String> pathVariableMap)  {
         return ("Fetched User with userId : " + pathVariableMap.get("userId") + " and phone number is : " + pathVariableMap.get("phNo"));
+=======
+        return ("User id is : " + userId + " and post id is : " + postId);
+
+>>>>>>> 2003ba7 (Assignment)
     }
 }
